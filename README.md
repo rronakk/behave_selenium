7ec5c8e5be4a4881ec54107f59a120f0b3c2154c
rray <knockronak@gmail.com>
2016-08-06 17:53:09 -0400

    initial commit with all changes

---
+++ README.md
@@ -0,0 +1,13 @@
+BDD_in_Python
+=============
+
+Repo for bdd testing with behave/nose
+
+Worked with Selenium for automated browser and Behave for test framework that utilizes 
+'Feature' files and testing 'Scenarios'. Each feature file has several scenarios that are then
+codified by test steps (step_impl()). 
+
+Hardest part was rerouting the form; short-term fix was just to go ahead and display the results route 
+but render the home.html template while passing in the form object with validators firing.
+
+Shoot me a message for details!
---
+++ coding_challenge/features/steps/amazon.py
@@ -0,0 +1,34 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import re
+
+@when(u'I go to the amazon home page')
+def step_impl(context):
+    context.browser.get('https://www.amazon.com')
+
+@when(u'I enter search text "{search_item}" in search field and click submit')
+def step_impl(context, search_item):
+    br = context.browser
+    search_field = br.find_element_by_id("twotabsearchtextbox")
+    search_field.send_keys(search_item)
+    br.find_element_by_class_name("nav-input").click()
+
+@then(u'I should see search result "{search_item}"')
+def step_imp(context, search_item):
+    br = context.browser
+    WebDriverWait(br, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".shoppingEngineSectionHeaders")))
+    assert "Amazon.com: " + search_item in br.title
+
+@then(u'Total number of search result is a positive integer')
+def step_imp(context):
+    br = context.browser
+    assert re.search(r'[\w\s,-]+', br.find_element_by_id("s-result-count").text)
+
+@then(u'No search result should be displayed')
+def step_imp(context):
+    br = context.browser
+
+    assert "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more" in br.title
+
+
